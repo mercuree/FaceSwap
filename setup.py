bf40dbf1b39998b20661fda44042d353638c4beb
Harry <mercuree.lab@gmail.com>
2019-03-30 21:10:09 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='FaceSwap',
+      version='1.0',
+      description='FaceSwap',
+      author='wuhuikai',
+      url='https://github.com/wuhuikai/FaceSwap',
+      packages=['FaceSwap']
+     )
